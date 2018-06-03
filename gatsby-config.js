b56ee0a0720e479f02262c299ff3a3ae7e7cde9f
Brad Murphy <bm.dev84@gmail.com>
2018-06-02 20:11:37 -0400

    Making progress on fetching wordpress

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,32 @@ module.exports = {
     title: 'Gatsby Default Redux Starter',
   },
   plugins: [
+    /*
+    * Gatsby's data processing layer begins with “source”
+    * plugins. Here the site sources its data from Wordpress.
+    */
+    {
+      resolve: 'gatsby-source-wordpress',
+      options: {
+        /*
+        * The base URL of the Wordpress site without the trailingslash and the protocol. This is required.
+        * Example : 'gatsbyjsexamplewordpress.wordpress.com' or 'www.example-site.com'
+        */
+        baseUrl: 'skwp.bradmurphydev.com',
+        // The protocol. This can be http or https.
+        protocol: 'http',
+        // Indicates whether the site is hosted on wordpress.com.
+        // If false, then the assumption is made that the site is self hosted.
+        // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
+        // If your site is hosted on wordpress.org, then set this to false.
+        hostingWPCOM: false,
+        // If useACF is true, then the source plugin will try to import the Wordpress ACF Plugin contents.
+        // This feature is untested for sites hosted on Wordpress.com.
+        // Defaults to true.
+        useACF: true,
+        verboseOutput: false,
+      },
+    },
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-typography`,
